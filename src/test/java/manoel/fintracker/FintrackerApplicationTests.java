63c54577d3b08098a08e48cd6a0db9d75adffa70
Manoel Junior <manuelitojunior@gmail.com>
2026-06-11 21:21:19 -0300

    initial config

---
+++ src/test/java/manoel/fintracker/FintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package manoel.fintracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
